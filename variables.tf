ac5e2b8c640a37de40abb17e2958a9f3330d1c38
Nick Pisciotta <npisciotta@gmail.com>
2023-04-06 09:00:22 -0400

    Include missing variable from tf variable file

--- variables.tf
+++ variables.tf
@@ -10,3 +10,4 @@ variable "cluster_name" {}
 variable "cluster_autoscaler_version" {}
 variable "metrics_server_version" {}
 variable "kube_state_metrics_chart_version" {}
+variable "aws_efs_csi_driver_chart_version" {}
